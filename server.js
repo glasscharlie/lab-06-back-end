6b3e2a63f08a125e8205ee6264ff68b80d094a41
Charles Glass <charlie-glass@live.com>
2019-11-05 12:15:18 -0800

    added locations

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/cool', (req, res) => {
+  res.send('cool data from the /cool route');
+});
+
+app.get('/location', (req, res) => {
+  //send users location back to them
+  const geoData = require('./data/geo.json');
+  const city = req.query.data;
+  const locationData = new Location(city, geoData);
+  res.send(locationData);
+});
+
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+
+}
+
 app.listen(PORT, () =>{
   console.log(`listening to PORT ${PORT}`);
 });
